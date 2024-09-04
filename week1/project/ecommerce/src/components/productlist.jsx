8c71282b7f1439e034b6b3f656234c7583734949
yousra elmaghraby <yousraelmaghraby@macbook-pro.home>
2024-09-03 18:03:54 -0700

    done

--- week1/project/ecommerce/src/components/productlist.jsx
+++ week1/project/ecommerce/src/components/productlist.jsx
@@ -8,7 +8,6 @@ const ProductList = ({ products }) => {
         <div key={product.id} className="product-card">
           <img src={product.image} alt={product.title} />
           <h3>{product.title.replace("FAKE: ", "")}</h3>
-          <p>{product.description}</p>
           <p>${product.price}</p>
           <p>Rating: {product.rating.rate} (Reviews: {product.rating.count})</p>
         </div>
